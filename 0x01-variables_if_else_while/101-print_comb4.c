80cb84206fd7f92178bbd1e3f0505504ec02cbef
Adoniyas7 <adoniyasg7s@gmail.com>
2021-08-27 11:24:44 -0700

    Fixed an error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-                                                                                                                                       
-/**
- * main - Prints numbers between 00 to 99. 
+ /**
+ * main - Prints numbers between 00 to 99.
  *
  * Return: Always 0 (Success)
  */
@@ -21,8 +20,7 @@ while (i < 58)
 g = 48;
 while (g < 58)
 {
-
- if (e != i && e != g && i != g && e < i && i < g)
+if (e != i && e != g && i != g && e < i && i < g)
 {
 putchar(e);
 putchar(i);
