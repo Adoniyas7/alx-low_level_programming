a646876a1c35b27f4c5f3ef39fcb19d9451b1d68
Adoniyas7 <adoniyasg7s@gmail.com>
2021-08-31 10:31:07 -0700

    Fixing file according to betty style

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - Prints the add of the even-valued
- *   * fibonacci numbers.
- *    *
- *     * Return: Always 0.
- *      */
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 long int n1, n2, fn, afn;
